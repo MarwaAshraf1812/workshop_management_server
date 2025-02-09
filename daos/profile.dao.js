4929cbe59341cee490ed5df3cb961b03cab93e13
Ahmed Mustafa <61328457+devAhmedMustafa@users.noreply.github.com>
2025-02-09 16:04:14 +0200

    Profile DAO: create profile, update profile

---
+++ daos/profile.dao.js
@@ -0,0 +1,45 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient()
+
+class ProfileDAO{
+    
+    static async createProfile(data){
+        try {
+            const profile = await prisma.profile.create({
+                data: {
+                    user_id: data.user_id,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                }
+            })
+
+            return profile;
+        } catch(err){
+            throw new Error(err.message)
+        }
+        finally{
+            await prisma.$disconnect()
+        }
+    }
+
+    static async updateProfile(userId, newUpdates){
+        try {
+            const profile = await prisma.profile.update({
+                where: { user_id: userId },
+                data: newUpdates,
+            })
+
+            return profile
+        } catch(err){
+            throw new Error(err.message)
+        }
+        finally{
+            await prisma.$disconnect()
+        }
+
+    }
+
+}
+
+module.exports = ProfileDAO
