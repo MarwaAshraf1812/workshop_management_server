a63a359c3c27b35e89c29d402e3b940b6b2049de
Ahmed Mustafa <61328457+devAhmedMustafa@users.noreply.github.com>
2025-02-09 16:25:47 +0200

    Profile validation: schema for both create & update

--- validations/profile.validation.js
+++ validations/profile.validation.js
@@ -1,9 +1,9 @@
 const Joi = require("joi");
 
-const createProfileSchema = Joi.object({
+const profileValidSchema = Joi.object({
     userId: Joi.string().required(),
     first_name: Joi.string().min(1).max(200),
     last_name: Joi.string().min(1).max(200),
 })
 
-module.exports = {createProfileSchema}
+module.exports = {profileValidSchema}
