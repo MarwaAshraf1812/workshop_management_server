6d88ddfc4e43f66bb6f6b194aefe5692b2b4cd9d
MarwaAshraf1812 <marashraf090@gmail.com>
2025-02-24 20:03:10 +0200

    test: test all material endpoints

--- routes/material.routes.js
+++ routes/material.routes.js
@@ -1,13 +1,26 @@
-
 const express = require("express");
-const MaterialController = require('../controllers/material.controller');
-const upload = require('../middlewares/fileUpload');
-const Authorization = require('../middlewares/Auth.middleware');
+const MaterialController = require("../controllers/material.controller");
+const upload = require("../middlewares/fileUpload");
+const { Authorization, ROLES } = require("../middlewares/Auth.middleware");
 const router = express.Router();
 
-// Routes
-router.post("/upload", upload.single('file'), MaterialController.uploadMaterial);
-router.get("/workshop/:workshopId", MaterialController.getMaterialsByWorkshopId);
-router.delete("/:id", MaterialController.deleteMaterial);
+router.post(
+  "/upload",  
+  Authorization.checkRoles(["INSTRUCTOR", "MODERATOR", "ADMIN"]),
+  upload.single("file"),
+  MaterialController.uploadMaterial
+);
+
+router.get(
+  "/:workshopId",
+  Authorization.checkRoles(["INSTRUCTOR", "MODERATOR", "ADMIN"]),
+  MaterialController.getMaterialsByWorkshopId
+);
+
+router.delete(
+  "/:materialId",
+  Authorization.checkRoles(["INSTRUCTOR", "MODERATOR", "ADMIN"]),
+  MaterialController.deleteMaterial
+);
 
 module.exports = router;
