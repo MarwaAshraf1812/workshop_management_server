da385e0b0b6a5c56f7c74ad4a93263e57cb7e3df
MarwaAshraf1812 <marashraf090@gmail.com>
2025-04-18 17:33:42 +0200

    comment limiter

--- app.js
+++ app.js
@@ -54,12 +54,12 @@ app.use(
  * to prevent DDoS attacks
  * and brute-force attacks 
  * */
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: "Too many requests from this IP, please try again later.",
-});
-app.use(limiter);
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 100,
+//   message: "Too many requests from this IP, please try again later.",
+// });
+// app.use(limiter);
 
 app.use(express.json());
 
